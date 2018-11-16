f6dd76287c5d93eec1afc33fafbe6dbd15e65e30
dekc89 <dekc89@gmail.com>
2018-11-16 16:13:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,5 +17,10 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  
+  // function incrementVariable() {
+  //   i = i + 1;
+  // }
+  do {
+    array.pop;
+  } while (array.length === 0)
 }
